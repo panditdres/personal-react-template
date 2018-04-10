8e572cfdfc00f04a27c31e11fd5c84c715cd7b84
pandit <pandit@takamarsukosair.home>
2018-04-10 14:13:06 +0100

    [master] update on the modal component

--- client/src/components/utils/Modal/index.js
+++ client/src/components/utils/Modal/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './index.css';
+import ReactDOM from 'react-dom';
 
-const Modal = ({children}) => (
+let Modal = ({children, onClose}) => ReactDOM.createPortal((
     <div className={styles.Modal}>
         <div className={styles.ModalBody}>
-            <div className={styles.ModalHeader}>Post a job to the board</div>
+            <div className={styles.ModalHeader}>
+                Edit Information
+                <img alt="" className={styles.closeBtn} onClick={onClose} src={Close}/>
+            </div>
             <div className={styles.ModalContent}>{children}</div>
         </div>
     </div>
-);
-
+),document.getElementById('modal-root'))
 export default Modal;
